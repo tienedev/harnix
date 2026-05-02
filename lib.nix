9ccab678f05799121d890eaf659ae85e47d015f4
tiene9 <tienedev@gmail.com>
2026-05-02 16:12:55 +0200

    feat: add lib helpers for plugin and MCP formatting

--- lib.nix
+++ lib.nix
@@ -1,1 +1,36 @@
-{ lib, pkgs }: {}
+{ lib, pkgs }:
+{
+  mkPluginDrv = { src, subpath, name }:
+    pkgs.runCommand "harnix-plugin-${name}" {} (
+      if subpath == "" || subpath == "."
+      then "cp -r ${src} $out"
+      else "cp -r ${src}/${subpath} $out"
+    );
+
+  mkSkillsDrv = { src, subpath, name }:
+    pkgs.runCommand "harnix-skills-${name}" {} (
+      let
+        skillsPath = if subpath == "" || subpath == "." then "${src}/skills" else "${src}/${subpath}/skills";
+      in ''
+        if [ -d "${skillsPath}" ]; then
+          cp -r "${skillsPath}" $out
+        else
+          mkdir $out
+        fi
+      ''
+    );
+
+  mkClaudeCodeMcpEntry = _name: server:
+    if server.type == "stdio" then
+      { inherit (server) type command args; }
+    else if server.type == "http" then
+      { inherit (server) type url; }
+      // lib.optionalAttrs (server.headers != {}) { inherit (server) headers; }
+    else  # sse
+      { inherit (server) type url; };
+
+  mkPiMcpEntry = _name: server: {
+    url       = server.url;
+    lifecycle = "lazy";
+  };
+}
